557f5fba53f1a285b8265af4429297de0d5001e5
ionut <ionutcioarec93@gmail.com>
2024-09-12 18:13:18 +0300

    added denominatedAmountToAmount instead of shiftBy to slippage calculation

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,9 +1,9 @@
 import BigNumber from 'bignumber.js';
 
-export const denominatedAmountToAmount = (amount: number, denomination: number, decimals: number) => {
+export const denominatedAmountToAmount = (amount: number | string, denomination: number, decimals: number) => {
   return new BigNumber(amount).shiftedBy(-denomination).decimalPlaces(decimals, BigNumber.ROUND_DOWN).toString(10);
 }
 
-export const amountToDenominatedAmount = (amount: number, denomination: number, decimals: number) => {
+export const amountToDenominatedAmount = (amount: number | string, denomination: number, decimals: number) => {
   return new BigNumber(amount).shiftedBy(denomination).decimalPlaces(decimals, BigNumber.ROUND_DOWN).toString(10);
 }
