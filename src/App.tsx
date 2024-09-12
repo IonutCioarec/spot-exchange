1fef588bd96c8bea6d7d963a8df9a9006d7e8166
ionut <ionutcioarec93@gmail.com>
2024-09-12 13:35:09 +0300

    Deleted the hardcoded route

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App: React.FC = () => {
       }}
       dappConfig={{
         shouldUseWebViewProvider: true,
-        logoutRoute: '/home'
+        logoutRoute: routeNames.home
       }}
       customComponents={{
         transactionTracker: {
