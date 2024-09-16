df85c5e78b28ad452cf43935ae73ec74a721732c
ionut <ionutcioarec93@gmail.com>
2024-09-17 02:05:48 +0300

    Added a first version of design for pools

--- src/components/Pools/Pool.tsx
+++ src/components/Pools/Pool.tsx
@@ -1,41 +1,87 @@
 import { Fragment } from "react/jsx-runtime";
 import 'assets/scss/pools.scss';
 import useMobile from 'utils/responsive';
-import { Pair } from "types/backendTypes";
+import { Pair, Token, TokenValue } from "types/backendTypes";
 import { useState } from "react";
 import { denominatedAmountToAmount } from 'utils/formatters';
 import { KeyboardArrowUp, KeyboardArrowDown } from '@mui/icons-material';
 import { Button } from "@mui/material";
+import { Row, Col } from "react-bootstrap";
+import { formatSignificantDecimals } from "utils/formatters";
 
-export const Pool = (pair: any) => {
+interface PoolProps {
+  pair: any;
+  index: number,
+  token1Details: TokenValue;
+  token2Details: TokenValue;
+}
+
+export const Pool = ({ pair, index, token1Details, token2Details }: PoolProps) => {
   const isMobile = useMobile();
   const [open, setOpen] = useState(false);
-  const token1_img = 'https://tools.multiversx.com/assets-cdn/devnet/tokens/WEGLD-a28c59/icon.png';
-  const token2_img = 'https://tools.multiversx.com/assets-cdn/devnet/tokens/MEX-a659d0/icon.png';
+  const token1DummyImg = 'https://tools.multiversx.com/assets-cdn/devnet/tokens/WEGLD-a28c59/icon.png';
+  const token2DummyImg = 'https://tools.multiversx.com/assets-cdn/devnet/tokens/MEX-a659d0/icon.png';
 
+  const token1LiqPrice = token1Details?.price ? pair.liquidity_token1 * token1Details?.price : 0;
+  const token2LiqPrice = token2Details?.price ? pair.liquidity_token2 * token2Details?.price : 0;
+  const tokensLiquidityPrice = token1LiqPrice && token2LiqPrice ? token1LiqPrice + token2LiqPrice : 0;
+
+  const token1Img = (token1Details?.logo_url && token1Details?.logo_url !== 'N/A') ? token1Details?.logo_url : token1DummyImg;
+  const token2Img = (token2Details?.logo_url && token2Details?.logo_url !== 'N/A') ? token2Details?.logo_url : token2DummyImg;
+
+  console.log(JSON.stringify(token1Details, null, 2));
   return (
     <Fragment>
       <div className="pool text-white">
-        <div className="d-flex justify-content-between align-items-center">
-          <div>
-            <img src={token1_img} alt={pair.token1} className='d-inline' style={{ width: 30, height: 30, marginRight: 5 }} />
-            <img src={token2_img} alt={pair.token2} className='d-inline' style={{ width: 30, height: 30, marginRight: 5 }} />
-            <span> WEGLD / MEX</span>
-          </div>
-          <div>
-            {denominatedAmountToAmount(parseFloat(pair.pair.liquidity_token1) + parseFloat(pair.pair.liquidity_token2), 18, 3)}
-          </div>
-          <div>
+        <Row className="align-items-center d-flex">
+          <Col lg={1} className="text-center">
+            {index + 1}
+          </Col>
+          <Col lg={1}>
+            <img src={token1Img} alt={pair.token1} className='d-inline' style={{ width: 30, height: 30 }} />
+            <img src={token2Img} alt={pair.token2} className='d-inline' style={{ width: 30, height: 30 }} />
+          </Col>
+          <Col lg={3}>
+            <div className="d-inline-grid mb-0">
+              <p className="mb-0">{token1Details?.ticker ? token1Details?.ticker : 'TOKEN1'}</p>
+              <p className="mt-0 mb-0 font-size-xxs text-silver">~ ${token1Details?.price ? formatSignificantDecimals(token1Details?.price) : '0'}</p>
+            </div>
+            <div className="d-inline-grid mx-2 mb-0">
+              <span>/</span>
+            </div>
+            <div className="d-inline-grid">
+              <p className="mb-0">{token2Details?.ticker ? token2Details?.ticker : 'TOKEN2'}</p>
+              <p className="mt-0 mb-0 font-size-xxs text-silver">~ ${token2Details?.price ? formatSignificantDecimals(token2Details?.price) : '0'}</p>
+            </div>
+          </Col>
+          <Col lg={2}>
+            <p className="mb-0 font-size-xxs text-silver">Liquidity</p>
+            ${denominatedAmountToAmount(tokensLiquidityPrice, 18, 3)}
+          </Col>
+          <Col lg={2} className="text-right">
+            <p className="mb-0 font-size-xxs text-silver">Volume(24h)</p>
+            $24.7k
+          </Col>
+          <Col lg={3} className="text-right">
             <Button
-              className="custom-btn"
+              className="custom-success-btn text-uppercase"
               variant="outlined"
               size="small"
+              color="success"
               onClick={() => setOpen(!open)}
             >
-              {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+              {open ? 'Collapse' : 'Expand'}
             </Button>
-          </div>
-        </div>
+            <Button
+              className="ms-2 custom-success-btn text-uppercase"
+              variant="outlined"
+              size="small"
+              color="success"
+            >
+              Add
+            </Button>
+          </Col>
+        </Row>
         {open && (
           <div className="mt-4" style={{ borderTop: '1px solid grey' }}>
             <p className="mt-1">Extra detalii... urmeaza</p>
--- src/pages/Pools.tsx
+++ src/pages/Pools.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { selectPairs, selectPairsStatus } from 'storeManager/slices/pairsSlice';
-import { selectPairTokens, selectLpTokens, selectTokensStatus } from 'storeManager/slices/tokensSlice';
+import { selectPairTokens, selectLpTokens, selectTokensStatus, selectPairTokensById } from 'storeManager/slices/tokensSlice';
 import Loader from 'components/Loader';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Pair } from 'types/backendTypes';
@@ -9,9 +9,10 @@ import { Pool } from 'components/Pools/Pool';
 
 const Pools = () => {
   const pairs = useSelector(selectPairs);
-  const pairtokens = useSelector(selectPairTokens);
+  const pairtokens = useSelector(selectPairTokensById);
   const lptokens = useSelector(selectLpTokens);
   const pairsStatus = useSelector(selectPairsStatus);
+  //console.log(JSON.stringify(pairs, null, 2));
 
   if (pairsStatus === 'loading') {
     return <Loader />;
@@ -22,8 +23,14 @@ const Pools = () => {
       <Row className='mb-5'>
         <Col xs={12} lg={12}>
           <div className='mt-5 mb-5'>
-            {pairs.map((pair: Pair, index: number) => (
-              <Pool pair={pair} key={`pairs-${index}`} />
+            {pairs.map((pair: any, index: number) => (
+              <Pool
+                key={`pairs-${index}`}
+                pair={pair}
+                index={index}
+                token1Details={pairtokens[pair?.token1]}
+                token2Details={pairtokens[pair?.token2]}
+              />
             ))}
           </div>
         </Col>
