98cdd86626dc90fc0eb2f5760c7b6cf999165ef7
ionut <ionutcioarec93@gmail.com>
2024-09-17 02:01:15 +0300

    Added a new function that will formata number to return first requested signifiant decimals (if requested decimals = 2: 0.00134 => 0.0013, 9.734 => 9.73)

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -7,3 +7,19 @@ export const denominatedAmountToAmount = (amount: number | string, denomination:
 export const amountToDenominatedAmount = (amount: number | string, denomination: number, decimals: number) => {
   return new BigNumber(amount).shiftedBy(denomination).decimalPlaces(decimals, BigNumber.ROUND_DOWN).toString(10);
 }
+
+export const formatSignificantDecimals = (input: number, decimals: number = 2): string => {
+  if (input === 0) {
+    return '0';
+  }
+
+  const regexPattern = new RegExp(`^-?\\d*\\.?0*\\d{0,${decimals}}`);
+  const fixedInput = input.toFixed(20);
+  const match = fixedInput.match(regexPattern);
+
+  if (match) {
+    return match[0];
+  } else {
+    return '0';
+  }
+};
