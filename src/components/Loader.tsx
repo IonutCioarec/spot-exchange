df86b13908adbd8ef22ff7a4d54bec549462d0bf
ionut <ionutcioarec93@gmail.com>
2024-09-18 02:37:45 +0300

    Updated the loader design

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,25 +1,30 @@
 import 'assets/css/loader.css';
 import { MoonLoader, PulseLoader } from 'react-spinners';
+import CircularProgress, {
+  circularProgressClasses,
+  CircularProgressProps,
+} from '@mui/material/CircularProgress';
 
 const Loader = () => {
   return (
     <div className='container mt-5' style={{ minHeight: '30vh' }}>
       <div className='flex flex-col p-6 items-center justify-center gap-2 rounded-xl bg-[#202020] w-full'>
         <div className='flex flex-col items-center'>
-          {/* White Spinner */}
-          <MoonLoader
-            color="#ffffff"
-            size={30}
-            speedMultiplier={0.5}
-            className='loader'
+          <CircularProgress
+            sx={{
+              color: '#01b574',
+              animationDuration: '2s',
+            }}
+            thickness={3}
+            size={35}
           />
 
           {/* Loading Text with Blinking Dots */}
-          <h4 className='mt-3 text-xl text-white flex'>
+          <h4 className='mt-3 text-xl text-[#01b574] flex'>
             Loading
             <span className='mt-1 ms-1'>
               <PulseLoader
-                color="#ffffff"
+                color="#01b574"
                 margin={3}
                 size={3}
                 speedMultiplier={0.6}
