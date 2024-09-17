51f7bbdf9e4607da128c809b296ef3cee277336a
ionut <ionutcioarec93@gmail.com>
2024-09-18 02:46:34 +0300

    Updated the expected pair type from Pools

--- src/pages/Pools.tsx
+++ src/pages/Pools.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { selectPairs, selectPairsStatus } from 'storeManager/slices/pairsSlice';
-import { selectPairTokens, selectLpTokens, selectTokensStatus, selectPairTokensById } from 'storeManager/slices/tokensSlice';
+import { selectLpTokens, selectPairTokensById } from 'storeManager/slices/tokensSlice';
 import Loader from 'components/Loader';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Pair } from 'types/backendTypes';
@@ -23,7 +23,7 @@ const Pools = () => {
       <Row className='mb-5'>
         <Col xs={12} lg={12}>
           <div className='mt-5 mb-5'>
-            {pairs.map((pair: any, index: number) => (
+            {pairs.map((pair: Pair, index: number) => (
               <Pool
                 key={`pairs-${index}`}
                 pair={pair}
