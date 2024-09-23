bec3a9227ecd3f702bc70484fac24e2c864aa225
ionut <ionutcioarec93@gmail.com>
2024-09-23 20:09:35 +0300

    Added a new type of mobile navbar

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,4 +1,3 @@
-
 import { routeNames } from 'routes/routes';
 import { Link, useLocation } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
@@ -6,9 +5,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import { useState } from 'react';
 import { Button } from '@mui/material';
 import 'assets/css/header.css';
-import { useGetIsLoggedIn } from 'hooks'
+import { useGetIsLoggedIn } from 'hooks';
 import { logout } from 'helpers';
-import {useMobile} from 'utils/responsive';
+import { useMobile } from 'utils/responsive';
+import 'assets/css/bottomNavbar.css';
+import HomeIcon from '@mui/icons-material/Home';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+import PoolIcon from '@mui/icons-material/Pool'; // Custom pool icon representation
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import LogoutIcon from '@mui/icons-material/Logout';
+import WorkspacesIcon from '@mui/icons-material/Workspaces';
 
 
 export const Header = () => {
@@ -24,12 +30,7 @@ export const Header = () => {
   const isLoggedIn = useGetIsLoggedIn();
   const handleLogout = () => {
     sessionStorage.clear();
-    logout(
-      callbackUrl,
-      onRedirect,
-      shouldAttemptReLogin,
-      options
-    );
+    logout(callbackUrl, onRedirect, shouldAttemptReLogin, options);
   };
 
   const isMobile = useMobile();
@@ -39,33 +40,142 @@ export const Header = () => {
 
   return (
     <>
-      <Navbar expanded={expanded} collapseOnSelect expand="lg" className="pl-6 pr-6 pt-2 pb-2" style={{ marginBottom: '0', minHeight: '72px', borderBottom: '1px solid #01b574' }}>
-        <Navbar.Brand as={Link} to={routeNames.home} className='pl-12 pr-4'>
+
+      <Navbar
+        expanded={expanded}
+        collapseOnSelect
+        expand="lg"
+        className="pl-6 pr-6 pt-2 pb-2"
+        style={{
+          marginBottom: '0',
+          minHeight: '72px',
+          borderBottom: '1px solid #01b574'
+        }}
+      >
+        <Navbar.Brand as={Link} to={routeNames.home} className="pl-12 pr-4">
           <p className={`text-brand ml-2 mb-0 mt-0 ${isMobile ? '' : 'ml-8'}`}>DEX</p>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" id='responsive-navbar-toggle' className="custom-toggler" onClick={handleToggle} />
-        <Navbar.Collapse id="responsive-navbar-nav" className='text-up2 pr-5 pl-5 text-center'>
-          <Nav className='text-center ml-auto' onSelect={handleSelect}>
-            <Nav.Link as={Link} to="/" className={`mx-1 ${location.pathname === '/' ? 'active' : ''}`} onClick={handleSelect}>
-              <p className='nav-link mb-0 mt-0 link'>Home</p>
-            </Nav.Link>
-            <Nav.Link as={Link} to="/swap" className={`mx-1 ${location.pathname === '/swap' ? 'active' : ''}`} onClick={handleSelect}>
-              <p className='nav-link mb-0 mt-0 link'>Swap</p>
-            </Nav.Link>
-            <Nav.Link as={Link} to="/pools" className={`mx-1 ${location.pathname === '/pools' ? 'active' : ''}`} onClick={handleSelect}>
-              <p className='nav-link mb-0 mt-0 link'>Pools</p>
-            </Nav.Link>
-          </Nav>
-          <Nav className={`ml-auto ${isMobile ? '' : 'mr-12'}`} onSelect={handleSelect}>
-            {!isLoggedIn ? (
-              <Button component={Link} to="/unlock" variant='contained' size='medium' color='success' className='custom-success-btn font-size-sm mb-0 mt-0'> Connect Wallet</Button>
-            ) : (
-              <Button onClick={handleLogout} variant='contained' size='medium' color='success' className='custom-success-btn font-size-xs mb-0 mt-0'> Disconnect</Button>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+        {!isMobile && (
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            id="responsive-navbar-toggle"
+            className="custom-toggler"
+            onClick={handleToggle}
+          />
+        )}
+        {!isMobile && (
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="text-up2 pr-5 pl-5 text-center"
+          >
+            <Nav className="text-center ml-auto" onSelect={handleSelect}>
+              <Nav.Link
+                as={Link}
+                to="/"
+                className={`mx-1 ${location.pathname === '/' ? 'active' : ''}`}
+                onClick={handleSelect}
+              >
+                <p className="nav-link mb-0 mt-0 link">Home</p>
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/swap"
+                className={`mx-1 ${location.pathname === '/swap' ? 'active' : ''}`}
+                onClick={handleSelect}
+              >
+                <p className="nav-link mb-0 mt-0 link">Swap</p>
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/pools"
+                className={`mx-1 ${location.pathname === '/pools' ? 'active' : ''}`}
+                onClick={handleSelect}
+              >
+                <p className="nav-link mb-0 mt-0 link">Pools</p>
+              </Nav.Link>
+            </Nav>
+            <Nav className={`ml-auto ${isMobile ? '' : 'mr-12'}`} onSelect={handleSelect}>
+              {!isLoggedIn ? (
+                <Button
+                  component={Link}
+                  to="/unlock"
+                  variant="contained"
+                  size="medium"
+                  color="success"
+                  className="custom-success-btn font-size-sm mb-0 mt-0"
+                >
+                  Connect Wallet
+                </Button>
+              ) : (
+                <Button
+                  onClick={handleLogout}
+                  variant="contained"
+                  size="medium"
+                  color="success"
+                  className="custom-success-btn font-size-xs mb-0 mt-0"
+                >
+                  Disconnect
+                </Button>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        )}
+        {isMobile && (
+          <Navbar
+            id="responsive-navbar-nav2"
+            className="text-up2 pr-5 pl-5 text-center mr-5"
+          >
+            <Nav className={`ml-auto ${isMobile ? '' : 'mr-12'}`} onSelect={handleSelect}>
+              {!isLoggedIn ? (
+                <Button
+                  component={Link}
+                  to="/unlock"
+                  variant="contained"
+                  size="medium"
+                  color="success"
+                  className="custom-success-btn font-size-sm mb-0 mt-0"
+                >
+                  Connect Wallet
+                </Button>
+              ) : (
+                <Button
+                  onClick={handleLogout}
+                  variant="contained"
+                  size="medium"
+                  color="success"
+                  className="custom-success-btn font-size-xs mb-0 mt-0"
+                >
+                  Disconnect
+                </Button>
+              )}
+            </Nav>
+          </Navbar>
+        )}
       </Navbar>
+      {isMobile && (
+        <div className="bottom-nav">
+          <div className="nav-items">
+            <Link to="/" className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+              <HomeIcon />
+              <p className='mb-0'>Home</p>
+            </Link>
+            <Link
+              to="/swap"
+              className={`nav-item ${location.pathname === '/swap' ? 'active' : ''}`}
+            >
+              <SwapHorizIcon />
+              <p className='mb-0'>Swap</p>
+            </Link>
+            <Link
+              to="/pools"
+              className={`nav-item ${location.pathname === '/pools' ? 'active' : ''}`}
+            >
+              <WorkspacesIcon />
+              <p className='mb-0'>Pools</p>
+            </Link>
+          </div>
+        </div>
+      )}
     </>
   );
 };
-
