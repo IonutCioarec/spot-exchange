5b796b3d61f8ad150870a97dd29ceabf7feeb7ef
ionut <ionutcioarec93@gmail.com>
2024-10-21 14:10:15 +0300

    Added a new package for glows, animations and other visual effects and created 2 reutilisable components, LightSpot and StaticLightSpot

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,6 +4,8 @@ import { Header } from './Header';
 import { Footer } from './Footer';
 import 'assets/css/layout.css';
 import 'assets/scss/layout.scss';
+import LightSpot from 'components/LightSpot';
+import StaticLightSpot from 'components/StaticLightSpot';
 
 interface LayoutProps {
   children: ReactNode;
@@ -21,6 +23,10 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <Footer />
       </main>
 
+      {/* Add light spots */}
+      <StaticLightSpot size={200} x="50%" y="40%" color="rgba(63, 172, 90, 0.3)" intensity={1}/>
+      <LightSpot size={300} x="10%" y="70%" color="rgba(63, 172, 90, 0.2)" blur={5}/>
+      <LightSpot size={250} x="85%" y="30%" color="rgba(63, 172, 90, 0.15)" />
 
     </div>
   );
