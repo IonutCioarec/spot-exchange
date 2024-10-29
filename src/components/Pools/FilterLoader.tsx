7224abd1396fdebe83ac731fdee50893f0ea6b50
IonutCioarec <ionutcioarec93@gmail.com>
2024-10-29 16:01:04 +0200

    Updated filter loader margins

--- src/components/Pools/FilterLoader.tsx
+++ src/components/Pools/FilterLoader.tsx
@@ -8,18 +8,16 @@ import CircularProgress, {
 
 const FilterLoader = () => {
   return (
-    <div className='' style={{ minHeight: '20vh' }}>
-      <div className='flex flex-col p-3 items-center justify-center gap-2 rounded-lg pool w-full'>
-        <div className='flex flex-col items-center mt-1'>
-          <CircularProgress
-            sx={{
-              color: '#3FAC5A',
-              animationDuration: '2s',
-            }}
-            thickness={3}
-            size={35}
-          />
-        </div>
+    <div className='flex flex-col p-3 items-center justify-center gap-2 rounded-lg pool w-full'>
+      <div className='flex flex-col items-center mt-1'>
+        <CircularProgress
+          sx={{
+            color: '#3FAC5A',
+            animationDuration: '2s',
+          }}
+          thickness={3}
+          size={35}
+        />
       </div>
     </div>
   );
