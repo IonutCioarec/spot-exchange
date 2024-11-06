e1f7a4f28ee0eda8a5aca69f71e763058c0e6fd4
IonutCioarec <ionutcioarec93@gmail.com>
2024-11-06 16:35:58 +0200

    Added the sort-by select to mobile

--- src/pages/Pools.tsx
+++ src/pages/Pools.tsx
@@ -248,7 +248,6 @@ const Pools = () => {
                           color: 'white',
                           fontFamily: 'Red Rose',
                           borderRadius: '15px',
-                          // boxShadow: '10px 0 7px rgba(63, 142, 90, 0.1), -10px 0 7px rgba(63, 142, 90, 0.1) !important'
                         },
                       },
                     }}
@@ -317,7 +316,7 @@ const Pools = () => {
               </div>
             </div>
             {isMobile &&
-              <div className='text-right m-t-n-sm'>
+              <div className='text-right m-t-n-sm d-flex align-items-center justify-content-end'>
                 <FormControlLabel
                   control={
                     <CustomSwitch
@@ -335,8 +334,48 @@ const Pools = () => {
                       fontSize: '14px',
                       fontFamily: 'Red Rose'
                     },
-                  }}
+                  }}                  
                 />
+                <span className='font-size-sm font-regular text-white m-r-n-sm'>Sort-by: </span>
+                  <Select
+                    id="sort-by"
+                    value={""}
+                    onChange={handleSortByChange}
+                    input={<OutlinedInput />}
+                    size='small'
+                    sx={{
+                      color: 'white',
+                      fontSize: '12px',
+                      fontFamily: 'Red Rose',
+                      padding: 0,
+                      '.MuiOutlinedInput-notchedOutline': {
+                        border: 'none',
+                      },
+                      '& .MuiSvgIcon-root': {
+                        color: 'white',
+                        marginLeft: '-50px !important'
+                      },
+                      backgroundColor: 'transparent',
+                    }}
+                    MenuProps={{
+                      PaperProps: {
+                        sx: {
+                          backgroundColor: 'rgba(32, 32, 32, 1)',
+                          color: 'white',
+                          fontFamily: 'Red Rose',
+                          borderRadius: '15px',
+                          // boxShadow: '10px 0 7px rgba(63, 142, 90, 0.1), -10px 0 7px rgba(63, 142, 90, 0.1) !important'
+                        },
+                      },
+                    }}
+                  >
+                    <MenuItem value="liquidity_desc" className='font-rose select-menu-item font-size-sm'>Highest Liquidity</MenuItem>
+                    <MenuItem value="liquidity_asc" className='font-rose select-menu-item font-size-sm'>Lowest Liquidity</MenuItem>
+                    <MenuItem value="volume24h_desc" className='font-rose select-menu-item font-size-sm'>Highest Volume 24h</MenuItem>
+                    <MenuItem value="volume24h_asc" className='font-rose select-menu-item font-size-sm'>Lowest Volume 24h</MenuItem>
+                    <MenuItem value="fees24h_desc" className='font-rose select-menu-item font-size-sm'>Highest Fees 24h</MenuItem>
+                    <MenuItem value="fees24h_asc" className='font-rose select-menu-item font-size-sm'>Lowest Fees 24h</MenuItem>
+                  </Select>
               </div>
             }
             {pairsStatus === 'loading' && <FilterLoader />}
