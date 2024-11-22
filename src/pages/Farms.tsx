e22b3ac6f1dba0787fd5d3c23959a335fe5c917e
IonutCioarec <ionutcioarec93@gmail.com>
2024-11-22 15:17:46 +0200

    Added the banner for the farms page

--- src/pages/Farms.tsx
+++ src/pages/Farms.tsx
@@ -4,14 +4,17 @@ import { Row, Col } from 'react-bootstrap';
 
 const Farms = () => {
   return (
-    <div className="farms-page-height">
-      <Container>
-        <Row className='d-flex text-white container align-items-center'>
-          <Col xs={12} lg={12}>
-
-          </Col>
-        </Row>
-      </Container>
+    <div className='farms-page-height'>
+      <Row>
+        <Col xs={12}>
+          <div className='b-r-sm d-flex align-items-center justify-content-center mt-4' style={{ backgroundColor: 'rgba(32,32,32, 0.5)', minHeight: '100px' }}>
+            <div className='p-5'>
+              <h2 className='text-white text-center'>Farms</h2>
+              <p className='text-white mb-0'>Stake your tokens in following farms and earn great rewards</p>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
