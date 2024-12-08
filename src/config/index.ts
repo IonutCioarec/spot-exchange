a1285827292617ef2072151e47eadc0f462ba447
IonutCioarec <ionutcioarec93@gmail.com>
2024-12-08 16:32:42 +0200

    Updated the devnet config file and removed the proxy from it

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,4 @@ export const API_URL = 'https://devnet-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
 export const contractAddress = '';
-// https://corsproxy.io/ (this is to bypass the no-cors errors on the localhost)
-export const dexAPI = 'https://corsproxy.io/?https://priv-backend.x-launcher.com/api';
+export const dexAPI = 'https://priv-backend.x-launcher.com/api';
