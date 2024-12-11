0f6837e9ed34be174760002c27c6b133ee92e648
IonutCioarec <ionutcioarec93@gmail.com>
2024-12-11 19:34:28 +0200

    Added new configs for create pool part

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -6,3 +6,21 @@ export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
 export const contractAddress = '';
 export const dexAPI = 'https://priv-backend.x-launcher.com/api';
+
+export const poolBaseTokens = {
+    token1: {
+        id: 'xEGLD',
+        ticker: 'xEGLD',
+        image: 'https://tools.multiversx.com/assets-cdn/devnet/tokens/EGLD/icon.png'
+    },
+    token2: {
+        id: 'USDC-350c4e',
+        ticker: 'USDC',
+        image: 'https://tools.multiversx.com/assets-cdn/devnet/tokens/USDC-350c4e/icon.png'
+    },
+    token3: {
+        id: 'XTICKET-6e9b83',
+        ticker: 'XTICKET',
+        image: 'https://tools.multiversx.com/assets-cdn/devnet/tokens/XTICKET-6e9b83/icon.svg'
+    },
+};
--- src/config/config.mainnet.ts
+++ src/config/config.mainnet.ts
@@ -6,3 +6,21 @@ export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.mainnet;
 export const contractAddress = '';
 export const dexAPI = '';
+
+export const poolBaseTokens = {
+    token1: {
+        id: '',
+        ticker: '',
+        image: ''
+    },
+    token2: {
+        id: '',
+        ticker: '',
+        image: ''
+    },
+    token3: {
+        id: '',
+        ticker: '',
+        image: ''
+    },
+};
