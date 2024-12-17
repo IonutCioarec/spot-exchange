90eb6e007ed3d6e3fcb44c883c704c004c8f0fc7
IonutCioarec <ionutcioarec93@gmail.com>
2024-12-17 15:22:47 +0200

    Created useSendAndSign hook that will be needed for transactions

---
+++ src/hooks/transactions/useSendAndSign.tsx
@@ -0,0 +1,30 @@
+import { Transaction } from '@multiversx/sdk-core/out';
+import { sendTransactions } from '@multiversx/sdk-dapp/services';
+import { TransactionsDisplayInfoType } from '@multiversx/sdk-dapp/types';
+import { refreshAccount } from '@multiversx/sdk-dapp/utils';
+ 
+export const sendAndSignTransactions = async (
+  transactions: Transaction[],
+  transactionsDisplayInfo: TransactionsDisplayInfoType,
+  minGasLimit = 20000000
+): Promise<{
+  success: boolean;
+  error: string;
+  sessionId: string | null;
+}> => {
+  try {
+    await refreshAccount();
+ 
+    const { sessionId, error } = await sendTransactions({
+      transactions: transactions.map((t) => t.toPlainObject()),
+      transactionsDisplayInfo,
+      minGasLimit
+    });
+ 
+    await refreshAccount();
+    return { success: error !== undefined, error: error ?? '', sessionId };
+  } catch (error: any) {
+    console.error(error);
+    return { success: false, error: error.message, sessionId: null };
+  }
+};
