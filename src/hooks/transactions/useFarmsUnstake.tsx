1145e38d4f826794cfa70fc88702847f332687b9
IonutCioarec <ionutcioarec93@gmail.com>
2024-12-20 11:55:02 +0200

    Created the farms unstake transaction hook

---
+++ src/hooks/transactions/useFarmsUnstake.tsx
@@ -0,0 +1,44 @@
+import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks/account';
+import { network, poolLPTokenPrice } from 'config';
+import { Transaction, BytesValue, Address, AddressValue, BigUIntValue, TokenTransfer } from '@multiversx/sdk-core/out';
+import { TransactionsDisplayInfoType } from '@multiversx/sdk-dapp/types';
+import { getFarmsSmartContractObj, sendAndSignTransactions, transactionDisplayInfo, watcher } from 'helpers';
+import BigNumber from 'bignumber.js';
+
+const sendAndSignTransactionsWrapped = async (
+  transactions: Transaction[],
+  displayInfo: TransactionsDisplayInfoType
+): Promise<{
+  success: boolean;
+  error: string;
+  sessionId: string | null;
+}> => {
+  const result = await sendAndSignTransactions(transactions, displayInfo);
+  await watcher.awaitCompleted(transactions[0]);
+  return result;
+};
+
+export const useFarmsUnstake = (token_id: string, token_decimals: number, token_amount: number) => {
+  const { account } = useGetAccountInfo();
+
+  const unstake = async () => {
+    const contract = await getFarmsSmartContractObj();
+    const interaction = contract.methodsExplicit.startFarming();
+
+    const transaction = interaction
+      .withNonce(account.nonce)
+      .withGasLimit(20_000_000)
+      .withChainID(network.chainId)
+      .withValue(0)
+      .withSingleESDTTransfer(TokenTransfer.fungibleFromAmount(token_id, token_amount, token_decimals))
+      .buildTransaction();
+
+    const sessionId = await sendAndSignTransactionsWrapped(
+      [transaction],
+      transactionDisplayInfo({ transactionName: 'unstake ', successTransactionName: 'Unstaked' })
+    );
+    return sessionId;
+  };
+
+  return unstake;
+};
