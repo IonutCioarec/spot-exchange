0207ff651d0c36883641120efbb3c271331ac510
IonutCioarec <ionutcioarec93@gmail.com>
2024-12-20 16:05:14 +0200

    Added new interface / type for mvx api calls

--- src/types/mvxTypes.ts
+++ src/types/mvxTypes.ts
@@ -3,3 +3,12 @@ export interface UserTokensState {
   userLpTokens: Record<string, { balance: string }>;
   status: 'loading' | 'succeeded' | 'failed';
 }
+
+export interface CreatedToken {
+  token_id: string;
+  ticker: string;
+  decimals: number;
+  logo: string;
+  balance: number;
+}
+export type CreatedTokens = Record<string, CreatedToken>;
