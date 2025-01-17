ced414b7eaeec68dd49063ea767ad64ff5bb3527
IonutCioarec <ionutcioarec93@gmail.com>
2025-01-17 18:42:18 +0200

    Added analytics header

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -5,13 +5,16 @@ import { Row, Col } from 'react-bootstrap';
 const Analytics = () => {
   return (
     <div className="analytics-page-height">
-      <Container>
-        <Row className='d-flex text-white container align-items-center'>
-          <Col xs={12} lg={12}>
-
-          </Col>
-        </Row>
-      </Container>
+      <Row id='topSection'>
+        <Col xs={12}>
+          <div className='b-r-sm d-flex align-items-center justify-content-center mt-4' style={{ backgroundColor: 'rgba(32,32,32, 0.5)', minHeight: '100px' }}>
+            <div className='p-5'>
+              <h2 className='text-white text-center'>Analytics</h2>
+              <p className='text-white text-justified mb-0'>Gain Insights with Our Analytics Page: Track trading volumes, liquidity trends, pools stats, etc to make informed decisions. Stay ahead with real-time data and historical analysis.</p>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
