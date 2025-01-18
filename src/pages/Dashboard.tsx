50b1da6e4a6eda87b986bfcec6ea7ccac4e630cc
IonutCioarec <ionutcioarec93@gmail.com>
2025-01-18 04:43:56 +0200

    Added dashboard page content header

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,13 +6,16 @@ import { Row, Col } from 'react-bootstrap';
 const Dashboard = () => {
   return (
     <div className="dashboard-page-height">
-      <Container>
-        <Row className='d-flex text-white container align-items-center'>
-          <Col xs={12} lg={12}>
-
-          </Col>
-        </Row>
-      </Container>
+      <Row id='topSection'>
+        <Col xs={12}>
+          <div className='b-r-sm d-flex align-items-center justify-content-center mt-4' style={{ backgroundColor: 'rgba(32,32,32, 0.5)', minHeight: '100px' }}>
+            <div className='p-5'>
+              <h2 className='text-white text-center'>Dashboard</h2>
+              <p className='text-white text-justified mb-0'>Stay informed with real-time updates on your earnings, staking progress, and overall portfolio health. Access all your stats in one place to make informed decisions and optimize your crypto journey.</p>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
