ba66624aa37063b3082f83fe14c760d04975f04f
IonutCioarec <ionutcioarec93@gmail.com>
2025-01-22 17:50:00 +0200

    Updated the config files to be tsx and added portofolio images and colors constant to it

---
+++ src/config/sharedConfig.tsx
@@ -0,0 +1,30 @@
+import AgricultureIcon from '@mui/icons-material/Agriculture';
+import WorkspacesIcon from '@mui/icons-material/Workspaces';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+
+export const walletConnectV2ProjectId = '6f6471a542f7f4a270ebacb54fc2b793';
+export const apiTimeout = 6000;
+export const transactionSize = 10;
+export const nativeAuth = true;
+export const adminAddresses = [
+  'erd1fegge5067awlw94ksycw0gfk8z2zzz2l3rnesjuytz8ampucsnwq5ns2hn'
+];
+export const stateLoaderRefreshTime = 15000;
+export const userStateLoaderRefreshTime = 5000;
+export const defaultSwapToken1 = 'WEGLD-a28c59';
+export const defaultSwapToken2 = 'MEX-a659d0';
+export const tokensItemsPerPage = 10;
+export const poolsItemsPerPage = 10;
+export const debounceSearchTime = 1000;
+
+export const portofolioImages = [
+  <AccountBalanceWalletIcon className='m-t-n-xxs' style={{ fontSize: '16px' }} />,
+  <WorkspacesIcon className='m-t-n-xxs' style={{ fontSize: '16px' }} />,
+  <AgricultureIcon className='m-t-n-xxs' style={{ fontSize: '16px' }} />
+];
+
+export const portofolioColors = [
+  '#3FAC5A',
+  '#0075ff',
+  '#4318ff'
+];
