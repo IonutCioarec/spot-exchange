597332f10e547e6bbf5909d99e053d69eeb9b30c
IonutCioarec <ionutcioarec93@gmail.com>
2025-03-01 17:36:31 +0200

    Created a dummy farms api response

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,88 @@
+import { FarmsState } from "types/backendTypes"
+
+export const farmsDummy: FarmsState = {
+  "TADAWEGLD-e427f1": {
+    "token1": "TADA-1e4ac8",
+    "token2": "WEGLD-a28c59",
+    "feesAPR": "20.21",
+    "boostedAPR": "10.05",
+    "totalAPR": "30.26",
+    "totalStaked": "1888999.22",
+    "totalRewards": "1987.28",
+    "stakingUsers": 234,
+    "lp_token_id": "TADAWEGLD-e427f1",
+    "totalRewardsList": [
+      {
+        token: 'TADAWEGLD-e427f1',
+        value: '1200'
+      },
+      {
+        token: 'PRIZEEGLD-e427f1',
+        value: '600'
+      }
+    ]
+  },
+  "CEGLDXCR-701cd5": {
+    "token1": "CEGLD-b22b50",
+    "token2": "XCR-010281",
+    "feesAPR": "10.51",
+    "boostedAPR": "11.72",
+    "totalAPR": "22.23",
+    "totalStaked": "698334.83",
+    "totalRewards": "865.63",
+    "stakingUsers": 119,
+    "lp_token_id": "CEGLDXCR-701cd5",
+    "totalRewardsList": [
+      {
+        token: 'CEGLDXCR-701cd5',
+        value: '1200'
+      },
+      {
+        token: 'PRIZEEGLD-e427f1',
+        value: '600'
+      }
+    ]
+  },
+  "XGTXCR-386762": {
+    "token1": "XGT-114d26",
+    "token2": "XCR-010281",
+    "feesAPR": "40.00",
+    "boostedAPR": "20.00",
+    "totalAPR": "60.00",
+    "totalStaked": "3549871.00",
+    "totalRewards": "10542.28",
+    "stakingUsers": 634,
+    "lp_token_id": "XGTXCR-386762",
+    "totalRewardsList": [
+      {
+        token: 'XGTXCR-386762',
+        value: '1200'
+      },
+      {
+        token: 'PRIZEEGLD-e427f1',
+        value: '600'
+      }
+    ]
+  },
+  "WTAOWEGLD-5833e2": {
+    "token1": "WTAO-a0cc6b",
+    "token2": "WEGLD-a28c59",
+    "feesAPR": "5.15",
+    "boostedAPR": "0.75",
+    "totalAPR": "5.90",
+    "totalStaked": "98754.31",
+    "totalRewards": "79.98",
+    "stakingUsers": 27,
+    "lp_token_id": "WTAOWEGLD-5833e2",
+    "totalRewardsList": [
+      {
+        token: 'WTAOWEGLD-5833e2',
+        value: '1200'
+      },
+      {
+        token: 'PRIZEEGLD-e427f1',
+        value: '600'
+      }
+    ]
+  }
+};
