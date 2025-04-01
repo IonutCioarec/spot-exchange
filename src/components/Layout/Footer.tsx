157fdac9c9879b34f8bc5a88572f99e410e9d43a
IonutCioarec <ionutcioarec93@gmail.com>
2025-04-01 20:01:53 +0300

    Updated footer with the new text logo

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,14 +1,25 @@
 import HeartIcon from 'assets/img/heart.svg?react';
+import textLogo from 'assets/img/logo_text.png';
+import { useMobile } from 'utils/responsive';
 
 export const Footer: React.FC = () => {
+  const isMobile = useMobile();
+  
   return (
     <footer className='text-white footer-area font-size-sm'>
       <div className='container d-flex justify-content-between align-items-center'>
         <div>
-          <p className='mb-0'>Copyright © 2024 <span className='text-intense-green'>Emeral<span className='half-colored-d'>D</span></span>EX</p>
+          <p className='mb-0'>Copyright © 2024
+            <img
+              src={textLogo}
+              alt='textLogo'
+              className='cursor-pointer d-inline'
+              style={{ height: 19 }}
+            />
+          </p>
         </div>
         <div className=''>
-          <span>Disclaimer: permissionless DEX, DYOR before purchasing!</span>          
+          <span>Disclaimer: permissionless DEX, DYOR before purchasing!</span>
         </div>
       </div>
     </footer>
