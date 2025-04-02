a2004f165c6d30111f60e55267edd55c8c100d94
IonutCioarec <ionutcioarec93@gmail.com>
2025-04-02 20:13:24 +0300

    Updated the admin addInitialLiquidity transaction with new parameters and a dynamic sc address

--- src/hooks/transactions/usePoolsAddInitialLiquidity.tsx
+++ src/hooks/transactions/usePoolsAddInitialLiquidity.tsx
@@ -2,7 +2,7 @@ import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks/account';
 import { network, poolLPTokenPrice } from 'config';
 import { Transaction, BytesValue, Address, AddressValue, BigUIntValue, TokenTransfer } from '@multiversx/sdk-core/out';
 import { TransactionsDisplayInfoType } from '@multiversx/sdk-dapp/types';
-import { getPairsSmartContractObj, sendAndSignTransactionsWrapped, transactionDisplayInfo, watcher } from 'helpers';
+import { sendAndSignTransactionsWrapped, transactionDisplayInfo, watcher, getDynamicPairsSmartContractObj } from 'helpers';
 import BigNumber from 'bignumber.js';
 
 interface TokenProps {
@@ -11,11 +11,11 @@ interface TokenProps {
   token_amount: number
 }
 
-export const usePoolsAddInitialLiquidity = (token1: TokenProps, token2: TokenProps) => {
-  const { account } = useGetAccountInfo();
+export const usePoolsAddInitialLiquidity = (scAddress:string, token1: TokenProps, token2: TokenProps) => {
+  const { account, address } = useGetAccountInfo();
 
   const addInitialLiquidity = async () => {
-    const contract = await getPairsSmartContractObj();
+    const contract = await getDynamicPairsSmartContractObj(scAddress);
     const interaction = contract.methodsExplicit.addInitialLiquidity();
 
     const transaction = interaction
@@ -23,6 +23,7 @@ export const usePoolsAddInitialLiquidity = (token1: TokenProps, token2: TokenPro
       .withGasLimit(20_000_000)
       .withChainID(network.chainId)
       .withValue(0)
+      .withSender(new Address(address))
       .withMultiESDTNFTTransfer([
         TokenTransfer.fungibleFromAmount(token1.token_id, token1.token_amount, token1.token_decimals),
         TokenTransfer.fungibleFromAmount(token2.token_id, token2.token_amount, token2.token_decimals)
