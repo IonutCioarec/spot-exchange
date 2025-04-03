097237591f046f72cd3e89ab8002f399216f949c
IonutCioarec <ionutcioarec93@gmail.com>
2025-04-03 16:46:14 +0300

    Created new router transaction to enable pair swap

---
+++ src/hooks/transactions/usePoolsResume.tsx
@@ -0,0 +1,29 @@
+import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks/account';
+import { network } from 'config';
+import { Address, AddressValue } from '@multiversx/sdk-core/out';
+import { getRouterSmartContractObj, sendAndSignTransactionsWrapped, transactionDisplayInfo } from 'helpers';
+
+export const usePoolsResume = (pair_address: string) => {
+  const { account } = useGetAccountInfo();
+
+  const resumePool = async () => {
+    const contract = await getRouterSmartContractObj();
+    const interaction = contract.methodsExplicit.resume([
+      new AddressValue(new Address(pair_address))
+    ]);
+
+    const transaction = interaction
+      .withNonce(account.nonce)
+      .withGasLimit(35_000_000)
+      .withChainID(network.chainId)
+      .withValue(0)
+      .buildTransaction();
+    const sessionId = await sendAndSignTransactionsWrapped(
+      [transaction],
+      transactionDisplayInfo({ transactionName: 'enable pool swap', successTransactionName: 'Pool swap enabled' })
+    );
+    return sessionId;
+  };
+
+  return resumePool;
+};
