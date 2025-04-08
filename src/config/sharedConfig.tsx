e99a3d5fa6a412f05b40727ac967bce086703cdf
IonutCioarec <ionutcioarec93@gmail.com>
2025-04-08 16:22:27 +0300

    Updated default swap tokens

--- src/config/sharedConfig.tsx
+++ src/config/sharedConfig.tsx
@@ -13,8 +13,8 @@ export const adminAddresses = [
 ];
 export const stateLoaderRefreshTime = 15000;
 export const userStateLoaderRefreshTime = 5000;
-export const defaultSwapToken1 = 'WEGLD-a28c59';
-export const defaultSwapToken2 = 'MEX-a659d0';
+export const defaultSwapToken1 = 'USDC-350c4e';
+export const defaultSwapToken2 = 'SPOT-ec8f71';
 export const tokensItemsPerPage = 10;
 export const poolsItemsPerPage = 10;
 export const debounceSearchTime = 1000;
