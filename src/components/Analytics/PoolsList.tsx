6a582f8a9c0ab021bb12c6c816ab5d6dcb81c463
IonutCioarec <ionutcioarec93@gmail.com>
2025-04-15 17:11:09 +0300

    Updated the analytics pool items images to have default images if no response or 'N/A' coming from the api

--- src/components/Analytics/PoolsList.tsx
+++ src/components/Analytics/PoolsList.tsx
@@ -24,9 +24,10 @@ import { selectPairsLimit, selectPairsPage, selectPairsSortBy, selectPairsSortDi
 import { selectNonZeroBalanceLpTokenIds } from 'storeManager/slices/userTokensSlice';
 import { debounce } from 'lodash';
 import { debounceSearchTime } from 'config';
+import defaultLogo from 'assets/img/no_logo.png';
 
 const defaultTokenValues = {
-  image_url: 'https://tools.multiversx.com/assets-cdn/devnet/tokens/WEGLD-a28c59/icon.png',
+  image_url: defaultLogo,
   name: 'TOKEN',
   price: 0,
   decimals: 18
@@ -290,13 +291,13 @@ const PoolsList: React.FC<PoolsListProps> = ({ pairs, allTokens}) => {
                     <td>
                       <div className="d-flex align-items-center" style={{ minWidth: isMobile ? '60px' : 'auto' }}>
                         <img
-                          src={allTokens[pair.token1]?.logo_url ?? defaultTokenValues.image_url}
+                          src={allTokens[pair.token1]?.logo_url && allTokens[pair.token1]?.logo_url !== 'N/A' ? allTokens[pair.token1].logo_url : defaultTokenValues.image_url}
                           alt={pair.token1}
                           className='d-inline'
                           style={{ width: 35, height: 35, border: '2px solid rgba(63, 172, 90, 0.3)', borderRadius: '20px' }}
                         />
                         <motion.img
-                          src={allTokens[pair.token2]?.logo_url ?? defaultTokenValues.image_url}
+                          src={allTokens[pair.token2]?.logo_url && allTokens[pair.token2]?.logo_url !== 'N/A' ? allTokens[pair.token1].logo_url : defaultTokenValues.image_url}
                           alt={pair.token2}
                           className="d-inline m-l-n-xxl"
                           initial={{ x: 0 }}
