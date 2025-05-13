f74338f0cd748ca3514515eb1bb4d93f8838578a
IonutCioarec <ionutcioarec93@gmail.com>
2025-05-13 17:43:11 +0300

    Updated the page not found styles

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -6,8 +6,8 @@ export const PageNotFound = () => {
   const { pathname } = useLocation();
 
   return (
-    <div className='container mt-5'>
-      <div className='flex flex-col p-6 items-center justify-center gap-2 rounded-xl bg-[#202020] w-full'>
+    <div className='swap-page-height py-5 mb-5'>
+      <div className='pt-12 mt-5 flex flex-col p-6 items-center justify-center gap-2 rounded-xl bg-[#202020] w-full'>
         <FontAwesomeIcon icon={faSearch} className='fa-3x mb-2 text-white' />
 
         <div className='flex flex-col items-center'>
