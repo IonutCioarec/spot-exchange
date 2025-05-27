5dd55e20d4fb99348d832b16b381e7dc0b361baa
IonutCioarec <ionutcioarec93@gmail.com>
2025-05-27 16:07:28 +0300

    Added the new created widgets for create_token and brand_token to the tools page and updated the page for the new structure

--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -8,6 +8,9 @@ import LightSpot from 'components/LightSpot';
 import { useNavigate } from 'react-router-dom';
 import CreateToken from 'components/Tools/CreateToken';
 import OwnedTokens from 'components/Tools/OwnedTokens';
+import createTokenImage from 'assets/img/create_token.png';
+import brandingTokenImage from 'assets/img/branding_token.png';
+import ToolWidget from 'components/Tools/ToolWidget';
 
 const Tools = () => {
   const isMobile = useMobile();
@@ -15,6 +18,17 @@ const Tools = () => {
   const isLoggedIn = useGetIsLoggedIn();
   const navigate = useNavigate();
 
+  const [openCreateToken, setOpenCreateToken] = useState(false);
+  const [openBrandingToken, setOpenBrandingToken] = useState(false);
+
+  const handleCreateToken = () => {
+    setOpenCreateToken(!openCreateToken);
+    setOpenBrandingToken(false);
+  };
+  const handleBrandingToken = () => {
+    setOpenBrandingToken(!openBrandingToken);
+    setOpenCreateToken(false);
+  };
 
   return (
     <div className="tools-page-height">
@@ -31,18 +45,47 @@ const Tools = () => {
         <ScrollToTopButton targetRefId='topSection' />
       )}
 
-      <Row className='mt-1 mb-5'>
-        {/* Create Token Section */}
-        <Col xs={12} lg={6} className='mt-2'>
-          <CreateToken />
+      {/* Tools Widgets */}
+      <Row className='mb-5'>
+        <Col xs={6} md={3} lg={2} className='mt-4'>
+          <ToolWidget
+            tokenImage={createTokenImage}
+            handleToken={handleCreateToken}
+            title='Create Token'
+            active={openCreateToken}
+          />
         </Col>
-
-        {/* Branding Tokens Section */}
-        <Col xs={12} lg={6} className='mt-2'>
-          <OwnedTokens />
+        <Col xs={6} md={3} lg={2} className='mt-4'>
+          <ToolWidget
+            tokenImage={brandingTokenImage}
+            handleToken={handleBrandingToken}
+            title='Brand Token'
+            active={openBrandingToken}
+          />
         </Col>
       </Row>
 
+      {/* Create Token Section */}
+      <Row>
+        <Col xs={12} md={{ offset: 2, span: 8 }} lg={{ offset: 3, span: 6 }} className='mt-2'>
+          <CreateToken
+            open={openCreateToken}
+            setOpen={handleCreateToken}
+          />
+        </Col>
+      </Row>
+
+      {/* Branding Tokens Section */}
+      <Row>
+        <Col xs={12} md={{ offset: 2, span: 8 }} lg={{ offset: 3, span: 6 }} className='mt-2'>
+          <OwnedTokens
+            open={openBrandingToken}
+            setOpen={handleBrandingToken}
+          />
+        </Col>
+      </Row>
+
+      <p className='my-5'>&nbsp;</p>
       {/* Add light spots */}
       <LightSpot size={isMobile ? 220 : 350} x={isMobile ? '25%' : '40%'} y="40%" color="rgba(63, 172, 90, 0.3)" intensity={1} />
     </div>
